2f55220a213d4cf784d3d904608114cdffe8e2b6
code-rain002 <jonny93229@gmail.com>
2020-04-29 17:58:28 +0900

    feat: semantic-uiのカスタマイズに対応

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "../../theme.config$": path.join(__dirname, "src/semantic/theme.config"),
+      },
+    },
+  });
+};
