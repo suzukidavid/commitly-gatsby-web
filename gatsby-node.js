3ab82a862acaa37f08bb14b2123f1c76c638464f
code-rain002 <jonny93229@gmail.com>
2020-05-11 22:09:25 +0900

    perf: ts-check追加

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,12 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
+
+require("ts-node").register({
+  compilerOptions: {
+    module: "commonjs",
+    target: "esnext",
+  },
+});
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   actions.setWebpackConfig({
