6de88b0803c06de39944d0b11ae8887f9a59ceaa
code-rain002 <jonny93229@gmail.com>
2020-05-12 23:42:45 +0900

    fix: 不要なファイル削除

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,15 +1,12 @@
 import { combineReducers } from "redux";
 
-import { AppState, appReducer } from "./app";
 import { AuthState, authReducer } from "./auth";
 
 export interface State {
-  app: AppState;
   auth: AuthState;
 }
 
 export const rootReducer = () =>
   combineReducers({
-    app: appReducer,
     auth: authReducer,
   });
